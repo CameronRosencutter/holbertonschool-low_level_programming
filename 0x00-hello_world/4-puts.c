acee9df93251c0b42833baafc174cbf693c78cfa
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-01-26 10:12:42 -0800

    Put it away

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,8 @@
 
 /**
  *  main - Entry Point
- *
- *  return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
