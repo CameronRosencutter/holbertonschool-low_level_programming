e9f6eb4acd48c89999e628094e52ab040615b73a
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-01 15:55:36 -0800

    haha

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,9 +10,8 @@ int main(void)
 {
 	char c;
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+	if (c >= 'C')
+		return (1);
+	else
+		return (0);
 }
