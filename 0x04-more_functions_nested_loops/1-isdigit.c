5ea976f4a56fa45c24df2266b63d043b582fe153
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-02 11:57:20 -0800

    pushhhh

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if digit or not
+ * @c: character
+ * Return: if digit (1), if not (0)
+ */
+
+int _isdigit(int c)
+{
+	char ch = '2';
+
+	if (_isdigit(ch))
+			return (1);
+			else
+			return (0);
+}
