282290a91c5729a81e6e387819dd7ac3acdc347c
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-02 13:15:08 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - multiply
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, mul;
+
+	printf("enter 2 numbers to multiply");
+	scanf("%d %d", &a, &b);
+	printf("Multiplacation = %d, mul");
+	return (0);
+}
