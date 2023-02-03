d620f323c1beaaaa773dccebf88d2653bf11e91c
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-03 12:07:54 -0800

    hjkj

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * print_numbers - Print numbers 1
+ * print_most_numbers - Print numbers 1
  * @c: char
  * Return: Always (0)
  */
 
-void print_numbers(void)
+void print_most_numbers(void);
 {
 	char c = '0';
 
 	while (c <= '9')
 	{
-		_putchar(c);
+		if (c != '2' && c != '4')
+		{
+			_putchar(c);
+		}
 		c++;
 	}
 	_putchar('\n');
