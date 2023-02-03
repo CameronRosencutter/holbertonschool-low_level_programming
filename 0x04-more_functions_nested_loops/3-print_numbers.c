03104c8bd60d672bbbda004e486b0d765a7ae16d
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-03 10:49:42 -0800

    jkljljli

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,8 +6,14 @@
  * Return: Always (0)
  */
 
-void print_numbers(void);
+void print_numbers(void)
 {
-	print_numbers(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
-	return (0);
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
