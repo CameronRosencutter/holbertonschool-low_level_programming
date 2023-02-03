f71950cc9c84e9d1222737ee10bba034b8480817
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-03 10:37:32 -0800

    1-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers 1
+ * @c - char
+ * Return: Always (0)
+ */
+
+void print_numbers(void);
+{
+	print_numbers(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+	return (0);
+}
