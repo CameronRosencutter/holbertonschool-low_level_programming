c61e84bf11fa2d1de4e30415d8926f62e0e80dcc
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-03 17:08:28 -0800

    Line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Print a straight line
+ * @n: characfjdaksjdfsia
+ */
+
+void print_line(int n)
+{
+	while (n == '9')
+		(n++);
+	{
+		_putchar('_');
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+}
