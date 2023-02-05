b0cbb517880cf200c77a98bd25b7d895d24bb8c9
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-05 12:46:26 -0800

    swapper

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+include "main.h"
+
+/**
+ * swap_int - Swap the values of two integers
+ * Return: always 0
+ * @a: a
+ * @b: b
+ */
+
+void swap_int(int *a, int *b)
+{
+	b = a / b;
+	a = a / b;
+	return (0);
+}
