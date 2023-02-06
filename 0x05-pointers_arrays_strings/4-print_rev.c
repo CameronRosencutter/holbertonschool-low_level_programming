7c2721668fc566216e5ee9a92bbd5a4786478691
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-06 14:52:58 -0800

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - Reverse
+ * Return: 0
+ * @s: character
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i--]);
+	}
+	_putchar('\n');
+}
