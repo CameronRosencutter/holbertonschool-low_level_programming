af88f6b529e96e5ade34c7b564dd2b33cd1560c3
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-06 15:32:41 -0800

    hjkhjk

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: character
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int start, temp;
+	int end = 0;
+
+	while (*(s + end) != '\0')
+	{
+		end++;
+	}
+
+	end -= 1;
+	start = 0;
+	while (end > start)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
