81e97139e2be59f4b7c987243fd57ec62c9cad81
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-19 17:46:45 -0800

    fff

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,17 +5,11 @@
  * @argc: total number of arguments
  * @argv: char pointers
  * Return: 0
- *
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s", argv[i]);
-	}
-	printf("\n");
+	printf("%d/n", argc - 1);
+	argv[0] = 0;
 	return (0);
 }
