a7b8d55cda7a7e2b06fe5dd69e55d1bc517c6b08
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-20 13:32:20 -0800

    ffff

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - mult 2 numbers
+ * @argc: argumetn Count
+ * @argv: argument
+ * Return: 0 succed, 1 failure
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	a = b = 0;
+	if (argc == 3)
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[1]);
+		printf("%d\n", a * b);
+		return (0);
+	}
+	else
+		return (1);
+}
