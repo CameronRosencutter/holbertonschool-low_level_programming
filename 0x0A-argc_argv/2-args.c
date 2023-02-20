befb8bf644c6888ddc3819a37787f1b850df6c59
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-20 10:20:59 -0800

    fff

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints all arguments it receives.
+ * @argc: size of argv
+ * @argv: name of app
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("argv[%2d]: %s\n", i, argv[i]);
+	return (0);
+}
