64b78032408c9e5388cbda7e6e1193fb4bdfcdc4
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-20 19:20:44 -0800

    sss

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - 2D grid
+ * @width: width of grid
+ * @height: height of grid
+ * Return: point 2D Grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **p;
+
+	i = j = 0;
+	if (height < 1)
+		return (NULL);
+	p = (int **)malloc(height * sizeof(p));
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		p[i] = malloc(width * sizeof(int));
+		if (p[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(p[j]);
+			free(p);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			p[i][j] = 0;
+	}
+	return (p);
+}
