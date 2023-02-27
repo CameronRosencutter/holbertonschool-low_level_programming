80c44261c11ffbde28492e19698d76a2d73eda1a
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-26 17:31:15 -0800

    dfsfds

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _calloc - change memory
+ * @nmemb: number of elements
+ * @size: Size of element
+ * Return: final
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *array;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	array = malloc(nmemb * size);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		*(array + (i * size)) = 0;
+	return (array);
+}
