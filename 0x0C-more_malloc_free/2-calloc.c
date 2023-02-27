91d0d4878e897e1295b784adc938e3dee0bab47e
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-02-26 17:39:05 -0800

    fdsafd

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,10 +17,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	array = malloc(nmemb * size);
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		*(array + (i * size)) = 0;
+
+	for (i = 0; i < (nmemb * size); i++)
+		array[i] = 0;
+
 	return (array);
 }
