8e9daa50ae57f74151b0ad12e9e9a4b9f64f70a2
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-03-17 18:32:31 -0700

    push

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints everythinng
+ * @h: Pointer to a linked list head
+ * Return: Nodes in linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	if (h == NULL)
+		return (0);
+	fir (i = 1; h->next != NULL; i++)
+	{
+		if (h->str == NULL)
+			printf("[%u] %s\n", h->len, "(nil)");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+	}
+	printf("[%u] %s\n", h->len, h->str);
+	return (i);
+}
