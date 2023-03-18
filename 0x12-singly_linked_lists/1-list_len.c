58b317b6b25985e4dfff60e2eeee0b4c3f9b710b
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-03-17 19:04:15 -0700

    fff

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - Find number of elements
+ * @h: linked list
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+
+	return (elements);
+}
