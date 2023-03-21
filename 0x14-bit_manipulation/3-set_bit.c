e7c49c1a324ee3aa5113f10c3e15f4291ae2f836
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-03-20 18:19:17 -0700

    fdsa

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *set_bit - sets the value of a bit to 1 at a given index
+ * @index: index
+ * Return: 1
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > ((sizeof(*n) * 8) - 1))
+		return (-1);
+	*n |= 1 << index;
+	return (1);
+}
